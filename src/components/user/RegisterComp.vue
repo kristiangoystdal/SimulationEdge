7cea66620a178907651a758507c72d9e15176b3b
Kristian Goystdal <krisgoy100@gmail.com>
2023-09-28 15:48:34 +0200

    Style Register

--- src/components/user/RegisterComp.vue
+++ src/components/user/RegisterComp.vue
@@ -1,49 +1,77 @@
 <template>
-    <div id="box">
-        <!-- Registration form -->
+
+    <v-sheet w-90 style="max-width: 400px" id="center" rounded >
         <div id="register">Register</div>
-    
-        <div id="form">
-            <!-- Email input -->
-            <div id="emailBox">
-                <input type="text" id="email" name="email" placeholder="E-mail" ref="email_signin" @input="validateEmail">
-            </div>
-            <!-- Password input -->
-            <div id="passwordBox1">
-                <input type="text" id="password" name="password" placeholder="Password" ref="password_signin" @input="passwordChecker">
-            </div>
-            <!-- Repeat Password input -->
-            <div id="passwordBox2">
-                <input type="text" id="password_check" name="passwordCheck" placeholder="Repeat Password" ref="password_signin" @input="passwordChecker">
-            </div>
-            <!-- Validation messages -->
-            <div id="errorMessages">
-                <div v-if="emailNotValidated">E-mail is not a valid e-mail address</div>
-                <div v-if="passwordDifferent">Password does not match</div>
-            </div>
-            <!-- Register button -->
-            <input type="submit" id="registerButton" name="register_Submit" value="Sign up" @click="register">
-        </div>
-  
-        <!-- Social login options -->
-        <div id="social">
-            <h1 class="registerWith">Sign in with:</h1><br>
-            <!-- Facebook register -->
-            <div class="social-signin_facebook" @click="signInWithFacebook">
-                <img src="../assets/photos/facebook_logo-transparent.png" alt="facebook_logo">
-            </div>
-            <!-- Google register -->
-            <div class="social-signin_google" @click="signInWithGoogle">
-                <img src="../assets/photos/google_logo.png" alt="google_logo">
-            </div>
-        </div>
-  
-        <!-- Link to login page -->
-        <div id="registerLink">
+
+        <v-form
+            v-model="form"
+            @submit.prevent="onSubmit"
+            id="registerForm"
+        >
+            <!-- E-mail Input field -->
+            <v-text-field
+            v-model="email"
+            :readonly="loading"
+            :rules="[rules.required, rules.email]"
+            class="mb-2"
+            clearable
+            label="Email"
+            ></v-text-field>
+
+            <!-- Password Input field -->
+            <v-text-field
+            v-model="password1"
+            :readonly="loading"
+            :rules="[rules.required, rules.min, rules.digit, rules.capital]"
+            clearable
+            label="Password"
+            placeholder="Enter your password"
+            hint="Must include a number, a capital letter and min 8 characters"
+            :type="show1 ? 'text' : 'password'"
+            :append-inner-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"     
+            @click:append-inner="show1=!show1"
+            >
+            </v-text-field>
+
+            <!-- Second Password Input field -->
+            <v-text-field
+            v-model="password2"
+            :readonly="loading"
+            :rules="[rules.required, rules.passwordmatch]"
+            clearable
+            label="Repeat Password"
+            placeholder="Enter your password"
+            :type="show2 ? 'text' : 'password'"
+            :append-inner-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"     
+            @click:append-inner="show2=!show2"
+            >
+            </v-text-field>
+
+            <br>
+
+            <v-btn
+            :disabled="!form"
+            :loading="loading"
+            block
+            color="success"
+            size="large"
+            type="submit"
+            variant="elevated"
+            @click="register"
+            >
+            Sign Up
+            </v-btn>
+        </v-form>
+
+        <br><br>
+        
+        <!-- Registration link -->
+        <div>
             <router-link to="/login">Login here</router-link>
         </div>
-    </div>
-  </template>
+    </v-sheet>
+    
+</template>
   
 <script>
     import {
@@ -64,124 +92,57 @@
         methods: {
             ...mapActions(['setUser']),
             
-            // Validate email format
-            validateEmail() {
-                const emailInput = document.getElementById("email").value;
-                // Regular expression to check if the input is a valid email address
-                const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-                return emailPattern.test(emailInput);
-            },
-            
-            // Check if passwords match
-            passwordChecker() {
-                return document.getElementById('password').value === document.getElementById('password_check').value;
-            },
-            
-            // Validate input and trigger registration
-            inputValidater() {
-                this.passwordDifferent = false;
-                this.emailNotValidated = false;
-
-                var faultyRegister = false
-        
-                if (!this.passwordChecker()) {
-                    this.passwordDifferent = true;
-                    faultyRegister =  true;
-                }
-                
-                if (!this.validateEmail()) {
-                this.emailNotValidated = true;
-                    faultyRegister = true;
-                }
-        
-                return !faultyRegister;
-            },
-            
             // Handle registration
             register() {
-                if (this.inputValidater()) {
-                    console.log("Check Done");
-                    const email_signin = this.$refs.email_signin.value;
-                    const password_signin = this.$refs.password_signin.value;
-            
-                    const auth = getAuth();
-                    // Set persistence to LOCAL to enable persistent authentication
-                    setPersistence(auth, browserLocalPersistence)
-                    .then(() => {
-                        // Create user with email and password
-                        return createUserWithEmailAndPassword(auth, email_signin, password_signin);
-                    })
-                    .then((userCredential) => {
-                        // Registration successful
-                        const user = userCredential.user;
-                        this.$emit('user', user);
-                        this.setUser(auth.currentUser);
-                        this.$router.push('/');
-                    })
-                    .catch((error) => {
-                        // Handle registration error
-                        console.log(error.code);
-                        alert(error.message);
-                    });
-                } 
-                else {
-                    console.log("Faulty register info");
-            
-                    // Shake animation
-                    const registerButton = document.getElementById('registerButton'); // Get the button by its id
-                    registerButton.classList.add('shake');
-            
-                    // Remove the shake class after the animation completes
-                    setTimeout(() => {
-                        registerButton.classList.remove('shake');
-                    }, 500); // Adjust the time (in milliseconds) to match your animation duration
-                }
-            },
-            
-            // Handle Google sign-in
-            signInWithGoogle() {
-                const provider = new GoogleAuthProvider();
-                signInWithPopup(getAuth(), provider)
+                if(!this.form) return
+
+                this.loading = true
+
+                setTimeout(() => (this.loading = false), 2000)
+
+                const email_signin = this.email;
+                const password_signin = this.password1;
+        
+                const auth = getAuth();
+                // Set persistence to LOCAL to enable persistent authentication
+                setPersistence(auth, browserLocalPersistence)
+                .then(() => {
+                    // Create user with email and password
+                    return createUserWithEmailAndPassword(auth, email_signin, password_signin);
+                })
                 .then((userCredential) => {
-                    // Login successful
+                    // Registration successful
                     const user = userCredential.user;
                     this.$emit('user', user);
-                    this.setUser(user);
+                    this.setUser(auth.currentUser);
                     this.$router.push('/');
                 })
                 .catch((error) => {
-                    // Handle login error
+                    // Handle registration error
                     console.log(error.code);
                     alert(error.message);
-                });
+                });   
             },
             
-            // Handle Facebook sign-in
-            signInWithFacebook() {
-                const provider = new FacebookAuthProvider();
-                signInWithPopup(getAuth(), provider)
-                .then((userCredential) => {
-                    // Login successful
-                    const user = userCredential.user;
-                    this.$emit('user', user);
-                    this.setUser(user);
-                    this.$router.push('/');
-                })
-                .catch((error) => {
-                    // Handle login error
-                    console.log(error.code);
-                    alert(error.message);
-                });
-                },
         },
         data() {
-        return {
-            passwordCheck: true,
-            passwordEmpty: true,
-            emailNotValidated: false,
-            passwordDifferent: false,
-            emptyInputs: false,
-        }
+            return {
+                form: false,
+                email: null,
+                password1: null,
+                password2: null,
+                loading: true,
+                show1: false,
+                show2: false,
+                rules:{
+                    required: value => !!value || 'Field is required',
+                    min: value => value.length >= 8 || 'Password must be min 8 characters',
+                    digit: value => (/\d/.test(value)) || 'Password must include a number',
+                    capital: value => (/[A-Z]/.test(value)) || 'Password must include a capital letter',
+                    email: value => (/.+@.+\..+/.test(value)) || 'Email must be valid',
+                    passwordmatch: value => value==this.password1 || "Password doesn't match",
+                },
+            }
         },
     };
   </script>
@@ -192,23 +153,28 @@ div{
     width: 100%;
     height: 100%;
 }
+
 .shake {
-        animation: shake 0.5s ease-in-out;
-    }
+    animation: shake 0.5s ease-in-out;
+}
 
-    @keyframes shake {
-        0% { transform: translateX(0); }
-        25% { transform: translateX(-5px); }
-        50% { transform: translateX(5px); }
-        75% { transform: translateX(-5px); }
-        100% { transform: translateX(5px); }
-    }
+@keyframes shake {
+    0% { transform: translateX(0); }
+    25% { transform: translateX(-5px); }
+    50% { transform: translateX(5px); }
+    75% { transform: translateX(-5px); }
+    100% { transform: translateX(5px); }
+}
 
-@media (max-width: 768px){
+#center{
+    margin: auto;
+}
+
+@media (min-width: 769px){
     #box{
         margin: 3vw auto;
         width: 90vw;
-        min-height: auto;
+        height: 80vh;
         /* border-color: green; */
         border-radius: 10px;
         border: solid;
@@ -216,6 +182,126 @@ div{
     }
 
     #register{
+        font-size: 3vw;
+        padding-top: 3vw;
+        padding-bottom: 3vw;
+        text-align: center;
+        font-family: 'TitleFont', sans-serif;
+        text-decoration: underline;
+        height: auto;
+    }
+
+    #registerForm{
+        width: 90%;
+        margin: auto;
+        height: auto;
+    }
+
+    #form{
+        padding: 5vw 0;
+        height: auto;
+        padding-bottom: 10vw;
+        width: 85%;
+        margin: auto;
+        border-bottom: none;
+    }
+
+    #social{
+        height: auto;
+        width: 100%;
+        margin: auto;
+        /* border: solid; */
+        border-top: dashed;
+        /* background-color: #ac19e1; */
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .loginWith{
+        display: none;
+        width: 90%;
+        font-size: 5vw;
+        text-decoration: underline;
+    }
+
+    .social-signin_facebook{
+        width: 20%;
+        height: auto;
+        margin: 2vw;
+        padding: 0.7vw;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border-radius: 50%;
+        border: solid;
+        background-color: #2f7af4;
+        cursor: pointer;
+    }
+    .social-signin_google{
+        height: auto;
+        width: 20%;
+        margin: 2vw;
+        padding: 1vw;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border-radius: 50%;
+        border: solid;
+        background-color: white;
+        cursor: pointer;
+    }
+
+
+    .social-text{
+        width: 70%;
+        font-size: 3.4vw;
+    }
+
+    img{
+        height: 100%;
+        width: 100%;
+    } 
+
+    #registerLink{
+        font-size: 6vw;
+        text-align: center;
+        width: 50%;
+        height: auto;
+        font-family: 'TitleFont', sans-serif;
+        padding-top: 0;
+        padding-bottom: 10vw;
+        margin: 5vw auto;
+        /* border: solid; */
+        align-items: center;
+        color: black;
+        text-decoration: underline;
+    }
+
+    #errorMessages{
+        background-color: #ff0000;
+        color: black;
+        font-size: 4vw;
+        text-align: center;
+        width: auto;
+        height: auto;
+        border-radius: 1vw;
+        font-family: 'TitleFont', sans-serif;
+    }
+
+}
+
+/* @media (max-width: 768px){
+    #box{
+        margin: 3vw auto;
+        width: 90vw;
+        height: 80vh;
+
+        border-radius: 10px;
+        border: solid;
+        background-color:#ac19e1;
+    }
+
+    #login{
         font-size: 10vw;
         padding-top: 15vw;
         padding-bottom: 15vw;
@@ -234,25 +320,24 @@ div{
         border-bottom: none;
     }
     #emailBox, #passwordBox1, #passwordBox2{
-        /* padding-left: 5vw;    */
+        
         width: 100%;  
         height: auto;
         margin: auto;
-        align-items: center;
-        display: flex;
         border-radius: 1vw;
+        display: flex;
     }
     
-    #username, #email, #password, #password_check{
-        width: 99%;
+    #username, #email, #password{
+        width: 100%;
         height: 10vw;
-        margin-bottom: 0.8vw;
+        margin: 0.5vw auto;
         font-size: auto;
         border-radius: 1vw;
         font-family: 'TitleFont', sans-serif;
     }
     
-    #registerButton{
+    #loginButton{
         width: 50%;
         height: auto;
         font-size: 5vw;
@@ -264,7 +349,6 @@ div{
         height: auto;
         width: 100%;
         margin: auto;
-        /* border: solid; */
         border-top: dashed;
         padding-top: 5vw;
         background-color: #ac19e1;
@@ -272,7 +356,7 @@ div{
         align-items: center;
         justify-content: center;
     }
-    .registerWith{
+    .loginWith{
         display: none;
         width: 90%;
         font-size: 5vw;
@@ -326,7 +410,6 @@ div{
         padding-top: 0;
         padding-bottom: 10vw;
         margin: 5vw auto;
-        /* border: solid; */
         align-items: center;
         color: black;
         text-decoration: underline;
@@ -345,7 +428,7 @@ div{
 
 
 
-    /* #register-box{
+    /* #login-box{
         margin: 3vw auto;
         width: 90vw;
         height: 100vw;
@@ -357,7 +440,7 @@ div{
         background-color:#ac19e1;
     }
 
-    #register-text{
+    #login-text{
         text-decoration: underline;
         font-size: 5vw;
         margin-top: 7vw;
@@ -380,14 +463,14 @@ div{
         margin: 0.5vw;
     }
 
-    #signup, #register{
+    #signup, #login{
         width: 70%;
         height: auto;
         font-size: 4vw;
         margin: 2vw auto;
     }
 
-    .registerWith{
+    .loginWith{
         width: 90%;
         font-size: 5vw;
         text-decoration: underline;
@@ -427,10 +510,10 @@ div{
         height: 50%;
         width: auto;
         margin: 4vw;
-    } */
+    }
 
 }
-/* #register-box{
+/* #login-box{
     margin: 3vw auto;
     width: 30vw;
     border-color: green;
@@ -440,7 +523,7 @@ div{
     background-color:#ac19e1;
 }
 
-#register-text{
+#login-text{
     text-decoration: underline;
     font-size: 2vw;
 }
@@ -461,13 +544,13 @@ div{
     margin: 0.5vw;
 }
 
-#signup, #register{
+#signup, #login{
     width: 70%;
     height: auto;
     font-size: 1.3vw;
 }
 
-.registerWith{
+.loginWith{
     width: 90%;
     font-size: 1.4vw;
     text-decoration: underline;
