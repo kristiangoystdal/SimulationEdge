c14dff35a5f4629616a3802c8bb8a160c01ac799
Kristian Goystdal <krisgoy100@gmail.com>
2023-09-29 08:45:11 +0200

    style cleanup

--- src/components/user/LoginComp.vue
+++ src/components/user/LoginComp.vue
@@ -227,7 +227,6 @@ div{
         margin: 3vw auto;
         width: 90vw;
         height: 80vh;
-        /* border-color: green; */
         border-radius: 10px;
         border: solid;
         background-color:#ac19e1;
@@ -323,7 +322,6 @@ div{
         padding-top: 0;
         padding-bottom: 10vw;
         margin: 5vw auto;
-        /* border: solid; */
         align-items: center;
         color: black;
         text-decoration: underline;
@@ -347,7 +345,6 @@ div{
         margin: 3vw auto;
         width: 90vw;
         height: 80vh;
-        /* border-color: green; */
         border-radius: 10px;
         border: solid;
         background-color:#ac19e1;
@@ -372,7 +369,6 @@ div{
         border-bottom: none;
     }
     #emailBox, #passwordBox1, #passwordBox2{
-        /* padding-left: 5vw;    */
         width: 100%;  
         height: auto;
         margin: auto;
@@ -401,7 +397,6 @@ div{
         height: auto;
         width: 100%;
         margin: auto;
-        /* border: solid; */
         border-top: dashed;
         padding-top: 5vw;
         background-color: #ac19e1;
@@ -463,7 +458,6 @@ div{
         padding-top: 0;
         padding-bottom: 10vw;
         margin: 5vw auto;
-        /* border: solid; */
         align-items: center;
         color: black;
         text-decoration: underline;
@@ -479,169 +473,5 @@ div{
         border-radius: 1vw;
         font-family: 'TitleFont', sans-serif;
     }
-
-
-
-    /* #login-box{
-        margin: 3vw auto;
-        width: 90vw;
-        height: 100vw;
-        border-color: green;
-        border-radius: 10px;
-        border: solid;
-        display: flex;
-        align-items: center;
-        background-color:#ac19e1;
-    }
-
-    #login-text{
-        text-decoration: underline;
-        font-size: 5vw;
-        margin-top: 7vw;
-    }
-
-    #left, #right{
-        width: 50%;
-        height: 100%;
-        text-align: center;
-        margin: 2vw auto;  
-    }
-    #left{
-        border-right: solid;
-        border-style: dashed;
-    }
-
-    #username, #email, #password{
-        width: 80%;
-        height: 5vw;
-        margin: 0.5vw;
-    }
-
-    #signup, #login{
-        width: 70%;
-        height: auto;
-        font-size: 4vw;
-        margin: 2vw auto;
-    }
-
-    .loginWith{
-        width: 90%;
-        font-size: 5vw;
-        text-decoration: underline;
-    }
-
-    .social-signin_facebook{
-        height: 10vw;
-        width: 90%;
-        margin: 1vw auto;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        border-radius: 2vw;
-        border: solid;
-        background-color: #2f7af4;
-        cursor: pointer;
-    }
-    .social-signin_google{
-        height: 10vw;
-        width: 90%;
-        margin: 1vw auto;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        border-radius: 2vw;
-        border: solid;
-        background-color: white;
-        cursor: pointer;
-    }
-
-    .social-text{
-        width: 70%;
-        font-size: 3.4vw;
-    }
-
-    img{
-        height: 50%;
-        width: auto;
-        margin: 4vw;
-    } */
-
 }
-/* #login-box{
-    margin: 3vw auto;
-    width: 30vw;
-    border-color: green;
-    border-radius: 10px;
-    border: solid;
-    display: flex;
-    background-color:#ac19e1;
-}
-
-#login-text{
-    text-decoration: underline;
-    font-size: 2vw;
-}
-
-#left, #right{
-    width: 50%;
-    text-align: center;
-    margin: 5px;  
-}
-#left{
-    border-right: solid;
-    border-style: dashed;
-}
-
-#username, #email, #password{
-    width: 80%;
-    height: 2vw;
-    margin: 0.5vw;
-}
-
-#signup, #login{
-    width: 70%;
-    height: auto;
-    font-size: 1.3vw;
-}
-
-.loginWith{
-    width: 90%;
-    font-size: 1.4vw;
-    text-decoration: underline;
-}
-
-.social-signin_facebook{
-    height: 3vw;
-    margin-top: 1vw;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-radius: 5px;
-    border: solid;
-    background-color: #2f7af4;
-}
-.social-signin_google{
-    height: 3vw;
-    margin-top: 1vw;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-radius: 5px;
-    border: solid;
-    background-color: white;
-}
-
-.social-text{
-    width: 70%;
-    font-size: 1.1vw;
-}
-
-img{
-    height: 50%;
-    width: auto;
-    margin: 1vw;
-} */
-
-
-
 </style>
