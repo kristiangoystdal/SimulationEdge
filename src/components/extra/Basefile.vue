314f82690d44ab115d58503ecf425cb31fd177a4
Kristian Goystdal <krisgoy100@gmail.com>
2023-10-12 21:28:52 +0200

    Basefile for Vue file

---
+++ src/components/extra/Basefile.vue
@@ -0,0 +1,29 @@
+<template>
+    <TitleVue :title='pageTitle'></TitleVue>
+</template>
+
+<script>
+    import TitleVue from '../extra/Title.vue';
+
+    export default {
+        name: 'Basefile',
+        data() {
+            return {
+                pageTitle: "PageTitle"
+            };
+        },
+        components: {
+            TitleVue
+        },
+    }
+</script>
+
+<style scoped>
+    @media (min-width: 769px) {
+
+    }
+
+    @media (max-width: 768px) {
+
+    }
+</style>
