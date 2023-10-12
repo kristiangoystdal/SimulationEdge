5930f2ae5387e6e152f68511577378bf561dadc3
Kristian Goystdal <krisgoy100@gmail.com>
2023-10-12 21:29:47 +0200

    New Ui and design for game

--- src/components/games/AlphabetGame.vue
+++ src/components/games/AlphabetGame.vue
@@ -1,15 +1,60 @@
 <template>
-    <v-container id="box" color="primary">
-      <div id="alphabet">
+    <TitleVue :title='pageTitle'></TitleVue>
+
+    <v-container id="box">
+      <div id="alphabet" class="alphabet-flex">
         <span v-for="(letter, index) in alphabet" :key="letter" :id="letter">
-          <div id="chip" :class="{ 'green-bg': index < currentIndex && input[index]==alphabet[index], 'red-bg': index < currentIndex && input[index]!=alphabet[index], 'white-bg': index >= currentIndex}">
+          <div id="chip" 
+              :class="{ 
+                'green-bg': index < currentIndex && input[index] == alphabet[index], 
+                'red-bg': index < currentIndex && input[index] != alphabet[index], 
+                'white-bg': index >= currentIndex 
+              }"
+              class="chip">
             {{ letter }}
           </div>
         </span>
       </div>
+      
     </v-container>
 
-    <div id="game">
+    <div id="other">
+      <div id="user">
+        <div id="timer">Time: {{ formattedTimer }} seconds</div>
+        <div id="done-text">{{ doneText }}</div>
+        <v-btn id="reset-button" @click="reset">
+          Reset
+        </v-btn>
+        
+      </div>
+      <div id="highscoreBoard">
+        <div id="highscoreTitle">Highscores</div>
+        <div id="type">
+          <div id="top">
+            <div id="topTitle">Top</div>
+            <div id="list">
+              <ul>
+                <li v-for="(item, index) in topScores" :key="index">
+                  {{ index + 1 }}. {{ item }}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div id="latest">
+            <div id="latestTitle">Latest</div>
+            <div id="list">
+              <ul>
+                <li v-for="(item, index) in latestScores" :key="index">
+                  {{ index + 1 }}. {{ item }}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- <div id="game">
       <div id="alphabet">
         <span v-for="(letter, index) in alphabet" :key="letter" :id="letter" :class="{ 'green-text': index < currentIndex }">{{ letter }}</span>
       </div>
@@ -23,10 +68,12 @@
       <div>
         <div id="reset-button" @click="reset">Reset</div>
       </div>
-    </div>
+    </div> -->
   </template>
   
 <script>
+  import TitleVue from '../extra/Title.vue';
+
   export default {
     data() {
       return {
@@ -38,10 +85,31 @@
         endTime: null,
         timer: 0,
         intervalId: null,
-        doneText: "",
-        win: null
-        
+        doneText: "Done",
+        win: null,
+        pageTitle: "The Alphabet Game",
+        topScores: [
+          "Top 1",
+          "Top 2",
+          "Top 3",
+          // Add your items here, up to 20
+        ],
+        latestScores: [
+          "Item 1",
+          "Item 2",
+          "Item 3",          
+          "Item 3",
+          "Item 3",
+          "Item 3",
+          "Item 3",
+
+          // Add your items here, up to 20
+        ],
+
       };
+    },
+    components: {
+      TitleVue
     },
     created() {
       this.startTimer();
@@ -141,6 +209,8 @@
     }
 
     #box{
+      max-width: 1200px;
+      width: 90%;
       border: solid;
       margin: auto;
       display: flex;
@@ -149,18 +219,116 @@
       border-radius: 20px;
     }
 
-    #chip{
+    .alphabet-flex {
+      display: flex;
+      flex-wrap: wrap; /* Allow items to wrap to the next row */
+      justify-content: center;
+      align-items: center;
+      max-width: 100%; /* Ensure it doesn't overflow the container */
+    }
+
+    #chip {
+      display: flex;
+      justify-content: center;
+      align-items: center;
       border-radius: 50%;
       width: 40px;
       height: 40px;
       text-align: center;
-      font-size: auto;
+      font-size: 20px; /* Replace 'auto' with a specific font size */
       border: solid;
     }
 
     #alphabet{
       display: flex;
+      width: 100%;
+      justify-content: center;
+      height: auto;
     }
+
+    #other{
+      display: flex;
+      max-width: 1200px;
+      justify-content: center;
+      margin: 2vw auto;
+      text-align: center;
+    }
+    #user{
+      width: 45%;  
+      height: auto;  
+      margin: 2vw auto;
+      margin-top: 0;
+      border: solid;
+      text-align: center;
+      border-radius: 20px;
+      background-color: bisque;
+    }
+    #highscoreBoard{
+      width: 45%;  
+      height: auto;  
+      margin: 2vw auto;
+      margin-top: 0;
+      border: solid;
+      text-align: center;
+      border-radius: 20px;
+      background-color: bisque;
+    }
+
+    #timer{
+      margin: 1vw;
+      font-size: 2vw;
+      font-family: 'TitleFont';
+    }
+    #reset-button{
+      width: 20%;
+      text-align: center;
+      padding: 0.5vw;
+      height: auto;
+      background-color: white;
+      border-radius: 2vw;
+      border: solid;
+    }
+    
+    #highscoreTitle{
+      margin: 1vw;
+      font-size: 2vw;
+      font-family: 'TitleFont';
+      text-decoration: underline;
+    }
+    #type{
+      display: flex;
+      margin: auto;
+    }
+    #top{
+      border-right: solid;
+      width: 50%;
+    }
+    #latest{
+      width: 50%;
+    }
+    #topTitle{
+      border-bottom: solid;
+      margin: 0.5vw 0;
+      font-size: 1.2vw;
+      font-family: 'TitleFont';
+      /* text-decoration: underline; */
+    }
+    #latestTitle{
+      border-bottom: solid;
+      margin: 0.5vw 0;
+      font-size: 1.2vw;
+      font-family: 'TitleFont';
+      /* text-decoration: underline; */
+    }
+    #list{
+      text-align: left;
+      padding-left: 10px;
+      padding-bottom: 20px;
+    }
+    li{
+      list-style: none;
+    }
+
 
     /* @media (min-width: 769px) {
         #game {
@@ -252,8 +420,4 @@
             border: solid;
         }
     } */
-    
-
-    
-    
 </style>
