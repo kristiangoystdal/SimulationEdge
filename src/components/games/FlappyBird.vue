3a967092c7edb4d544ec82705a37244732d3886c
Kristian Goystdal <krisgoy100@gmail.com>
2023-11-08 13:52:24 +0100

    started work on "FlappyBird" game

---
+++ src/components/games/FlappyBird.vue
@@ -0,0 +1,29 @@
+<template>
+    <TitleVue :title='pageTitle'></TitleVue>
+</template>
+
+<script>
+    import TitleVue from '../extra/Title.vue';
+
+    export default {
+        name: 'Basefile',
+        data() {
+            return {
+                pageTitle: "Flappy Bird"
+            };
+        },
+        components: {
+            TitleVue
+        },
+    }
+</script>
+
+<style scoped>
+    @media (min-width: 769px) {
+
+    }
+
+    @media (max-width: 768px) {
+
+    }
+</style>
---
+++ src/pages/games/FlappyBird.vue
@@ -0,0 +1,22 @@
+<template>
+    <FlappyBird></FlappyBird>
+    <!-- <HighscoreComp></HighscoreComp> -->
+</template>
+    
+<script>
+    import FlappyBird from '../../components/games/FlappyBird.vue'
+    // import HighscoreComp from '../../components/HighscoreComp.vue'
+
+    export default{
+        name: "FlappyBirdPage", 
+        components: {
+            FlappyBird, 
+            // HighscoreComp,
+        }
+    }
+</script>
+    
+<style scoped>
+
+</style>
+    
