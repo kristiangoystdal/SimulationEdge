4592ac126b0ddd328e0b12e48d2fa4f782a463e8
Kristian Goystdal <krisgoy100@gmail.com>
2023-11-13 22:56:02 +0100

    added user tools to account page

--- src/pages/main/Account.vue
+++ src/pages/main/Account.vue
@@ -1,17 +1,20 @@
 <template>
   <UserData></UserData>
+  <UserTools></UserTools>
   <SignOut></SignOut>
 </template>
   
   <script>
     import SignOut from '../../components/user/SignOut.vue'
     import UserData from '../../components/user/UserData.vue'
+    import UserTools from '../../components/user/UserTools.vue';
 
     export default{
       name: "AccountPage", 
       components: {
-        SignOut, UserData,
-      }
+    SignOut, UserData, UserTools,
+    UserTools
+}
     }
   </script>
   
