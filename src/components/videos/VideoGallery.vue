14cd396b67040f222e042a0aef75c18bf7ee318f
Kristian Goystdal <krisgoy100@gmail.com>
2023-11-16 22:38:53 +0100

    new layout for video gallery

--- src/components/videos/VideoGallery.vue
+++ src/components/videos/VideoGallery.vue
@@ -1,28 +1,41 @@
 <template>
     <TitleVue :title="pageTitle"></TitleVue>
-  
-    <div id="gallery" v-if="!videoPlaying">
-        <v-carousel :cycle="!videoPlaying" hide-delimiter-background show-arrows="hover" class="carousel">
-            <v-carousel-item v-for="(slide, i) in slideData" :key="i" @click="playVideo(i)">
-                <v-sheet >
-                <v-img :src="slide.image" :aspect-ratio="16/9" cover>
-                    <div class="text-h2" @click.stop="playVideo(i)">
-                      {{ !videoPlaying ? slide.name : '' }}
-                      <v-icon v-if="!videoPlaying" class="play-button" @click.stop="playVideo(i)">mdi-play-circle-outline</v-icon>
-                    </div>
-                </v-img>
-                </v-sheet>
-            </v-carousel-item>
-        </v-carousel>
-    </div>
-    <div id="gallery" v-if="videoPlaying">
-      <video controls autoplay loop ref="videoPlayer" class="video-player" @click.stop="toggleVideo">
-        <source :src="currentVideo" type="video/mp4">
-        Your browser does not support the video tag.
-      </video>
+
+    <div id="gallery">
+      <v-container>
+        <v-row>
+          <v-col v-for="(item, index) in slideData" :key="index" cols="12" sm="6" md="4" lg="3">
+            <v-sheet>
+              <v-img
+                v-if="!item.videoVisible"
+                :src="item.image"
+                :alt="item.name"
+                @click="toggleMedia(index)"
+                aspect-ratio="16/9"
+                cover
+              >
+                <v-icon v-if="!item.videoVisible" class="play-button">mdi-play-circle-outline</v-icon>
+              </v-img>
+
+              <video
+                v-if="item.videoVisible"
+                :src="item.video"
+                :autoplay="item.videoVisible"
+                controls
+                loop
+                @click="toggleMedia(index)"
+                ref="videoPlayer{{ index }}"
+              >
+                Your browser does not support the video tag.
+              </video>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
     </div>
 
-    <br><br><br>
+    <br><br>
+    {{ mobile }}
   </template>
   
   <script>
@@ -42,9 +55,9 @@
     data() {
       const slideData = [
         // { name: 'Lofoten', image: LofotenImage, video: LofotenVideo },
-        { name: 'Åre', image: AreImage, video: AreVideo },
+        { name: 'Åre', image: AreImage, video: AreVideo, videoVisible: false},
         // { name: 'Roadtrip i USA', image: USAImage, video: USAVideo },
-        { name: 'Trehyttetur', image: TrehytteImage, video: TrehytteVideo },
+        { name: 'Trehyttetur', image: TrehytteImage, video: TrehytteVideo, videoVisible: false},
       ];
   
       return {
@@ -59,11 +72,40 @@
       TitleVue,
     },
     methods: {
+      toggleMedia(index) {
+        const item = this.slideData[index];
+        for(var i = 0; i<this.slideData.length; i++){
+          this.slideData[i] = { ...this.slideData[i], videoVisible: false};
+        }
+        if (item) {
+          // Directly modify the array
+          this.slideData[index] = { ...item, videoVisible: !item.videoVisible };
+
+          // Access the video element by ref
+          const videoElement = this.$refs[`videoPlayer${index}`];
+
+          if (videoElement) {
+            if (item.videoVisible) {
+              videoElement.play();
+            } else {
+              videoElement.pause();
+            }
+          }
+
+          // // Trigger a re-render
+          // this.$forceUpdate();
+        } else {
+          console.error(`Item at index ${index} is undefined.`);
+        }
+      },
       playVideo(index) {
         if (this.videoPlaying) {
           this.stopVideo();
         } else {
-          this.videoPlaying = true;
+          if(!this.mobile)
+          {
+            this.videoPlaying = true;
+          }
           this.activeVideoIndex = index;
           this.currentVideo = this.slideData[index].video;
           this.$nextTick(() => {
@@ -91,6 +133,17 @@
         }
       },
     },
+    computed:{
+      mobile(){
+        if(innerWidth<769){
+          return true
+        }
+        else{
+          return false
+        }
+      }
+      
+    },
   };
   </script>
   
@@ -102,55 +155,124 @@
     font-style: normal;
     src: url('../../assets/fonts/FredokaOne-Regular.ttf') format('truetype');
   }
-  
-  #gallery {
-    max-width: 900px;
-    width: 90%;
-    height: auto;
-    border: solid;
-    margin: auto;
-  }
-  
-  .carousel{
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    background-color: black;
-  }
-  
-  .text-h2 {
-    position: absolute;
-    top: 40%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    color: white;
-    text-align: center;
-    font-size: 10vw;
-    font-weight: bold;
-    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
-    font-family: 'TitleFont', sans-serif;
-    cursor: pointer;
-  }
-  
+
   .play-button {
+    font-size: 3em;
+    color: white;
     position: absolute;
-    top: 150%;
+    top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    font-size: 4vw;
-    color: rgba(255, 255, 255, 0.8);
     cursor: pointer;
   }
   
-  .play-button:hover {
-    color: white;
+  @media(min-width: 769px){
+    #gallery {
+      max-width: 900px;
+      width: 90%;
+      height: auto;
+      border: solid;
+      margin: auto;
+    }
+    
+    .carousel{
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      background-color: black;
+    }
+    
+    .text-h2 {
+      position: absolute;
+      top: 40%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: white;
+      text-align: center;
+      font-size: 10vw;
+      font-weight: bold;
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+      font-family: 'TitleFont', sans-serif;
+      cursor: pointer;
+    }
+    
+    .play-button {
+      position: absolute;
+      top: 150%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      font-size: 4vw;
+      color: rgba(255, 255, 255, 0.8);
+      cursor: pointer;
+    }
+    
+    .play-button:hover {
+      color: white;
+    }
+    
+    .video-player {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      background-color: black;
+    }
+  }
+
+  @media(max-width: 768px){
+    #gallery {
+      max-width: 900px;
+      width: 90%;
+      height: auto;
+      border: solid;
+      margin: auto;
+    }
+    
+    .carousel{
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      background-color: black;
+    }
+    
+    .text-h2 {
+      position: absolute;
+      top: 40%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: white;
+      text-align: center;
+      font-size: 10vw;
+      font-weight: bold;
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+      font-family: 'TitleFont', sans-serif;
+      cursor: pointer;
+    }
+    
+    .play-button {
+      position: absolute;
+      top: 150%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      font-size: 4vw;
+      color: rgba(255, 255, 255, 0.8);
+      cursor: pointer;
+    }
+    
+    .play-button:hover {
+      color: white;
+    }
+    
+    .video-player {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      background-color: black;
+    }
+    video{
+      width: 100%;
+      height: 100%;
+    }
   }
   
-  .video-player {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    background-color: black;
-  }
   </style>
   
