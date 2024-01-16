13521f63b4b5f12fed962e5a91b1f3f16def8727
Kristian Goystdal <krisgoy100@gmail.com>
2024-01-16 20:17:08 +0100

    fixed month of saved scores

--- src/components/games/SimonSays.vue
+++ src/components/games/SimonSays.vue
@@ -2,47 +2,28 @@
     <TitleVue :title="pageTitle"></TitleVue>
     <div id="box">
         <div id="lightBox">
-            <v-btn
-                v-for="(light, index) in lights"
-                :key="index"
-                :id="'light-' + (index + 1)"
-                :style="getLightStyle(light,index)"
-                @click="buttonPress(index)"
-                :class="{'disabledLight': currentlyBlinking || gameover,'light' : !currentlyBlinking && !gameover }"
-            >
+            <v-btn v-for="(light, index) in lights" :key="index" :id="'light-' + (index + 1)"
+                :style="getLightStyle(light, index)" @click="buttonPress(index)"
+                :class="{ 'disabledLight': currentlyBlinking || gameover, 'light': !currentlyBlinking && !gameover }">
                 {{ index + 1 }}
             </v-btn>
         </div>
         <div id="switchBox">
             <div id="switch">
-                <v-switch
-                v-model="autoRound"
-                hide-details
-                inset
-                :label="`Auto run: ${autoRound}`"
-                true-value="On"
-                false-value="Off"
-            ></v-switch>
+                <v-switch v-model="autoRound" hide-details inset :label="`Auto run: ${autoRound}`" true-value="On"
+                    false-value="Off"></v-switch>
             </div>
-            
+
         </div>
-        
+
     </div>
-    
 
-    <HighscoreComp 
-        :scoreTitle='scoreTitle' 
-        :userScoresPath="userPath"
-        :databasePath="dbPath" 
-        :buttonFunction="blinkLights" 
+
+    <HighscoreComp :scoreTitle='scoreTitle' :userScoresPath="userPath" :databasePath="dbPath" :buttonFunction="blinkLights"
         :buttonText="startText"
-        :buttonDisable="!currentlyPlaying && (autoRound=='Off' || sequenceLength!=0) || gameover || (autoRound=='On' && sequenceLength==0) "
-        :score="sequenceLength-1"
-        :scoreLabel="label"
-        :sortWay="hightoLow"
-    >
-    </HighscoreComp> 
-
+        :buttonDisable="!currentlyPlaying && (autoRound == 'Off' || sequenceLength != 0) || gameover || (autoRound == 'On' && sequenceLength == 0)"
+        :score="sequenceLength - 1" :scoreLabel="label" :sortWay="hightoLow">
+    </HighscoreComp>
 </template>
 
 <script>
@@ -85,23 +66,23 @@ export default {
         TitleVue,
         HighscoreComp,
     },
-    computed:{
+    computed: {
         blinkDuration() {
-            if(this.sequenceLength>12){
+            if (this.sequenceLength > 12) {
                 return 300
             }
-            else{
-                return 600-(25*(this.sequenceLength-1));
+            else {
+                return 600 - (25 * (this.sequenceLength - 1));
             }
         },
         user() {
             return this.$store.getters.getCurrUser;
-        },  
-        startText(){
-            if(this.gameover){
+        },
+        startText() {
+            if (this.gameover) {
                 return "Start New Round"
             }
-            else{
+            else {
                 return "Start Round " + this.sequenceLength.toString()
             }
         },
@@ -109,16 +90,16 @@ export default {
     methods: {
         async blinkLights() {
             await new Promise(resolve => setTimeout(resolve, 200));
-            if(this.gameover){
+            if (this.gameover) {
                 this.reset()
-                if(this.autoRound=="On"){
+                if (this.autoRound == "On") {
                     this.blinkLights();
                 }
             }
-            else{
-                this.currentlyBlinking= true;
-                this.currentlyPlaying=true;
-                this.sequenceIndex=0;   
+            else {
+                this.currentlyBlinking = true;
+                this.currentlyPlaying = true;
+                this.sequenceIndex = 0;
                 // Generate a random index to change the light color
                 const randomIndex = Math.floor(Math.random() * this.lights.length);
                 this.sequence.push(randomIndex);
@@ -137,58 +118,95 @@ export default {
                         await new Promise(resolve => setTimeout(resolve, this.blinkDuration));
                     }
                 }
-                this.currentlyBlinking=false;
+                this.currentlyBlinking = false;
             }
         },
-        buttonPress(index){
-            if(!this.currentlyBlinking && !this.gameover){
-                if(index==this.sequence[this.sequenceIndex]){
+        buttonPress(index) {
+            if (!this.currentlyBlinking && !this.gameover) {
+                if (index == this.sequence[this.sequenceIndex]) {
                     this.sequenceIndex++;
-                    if(this.sequenceIndex==this.sequenceLength){
+                    if (this.sequenceIndex == this.sequenceLength) {
                         this.sequenceLength++;
-                        this.currentlyPlaying=false;
-                        this.currentlyBlinking=true;
-                        if(this.autoRound=="On"){
+                        this.currentlyPlaying = false;
+                        this.currentlyBlinking = true;
+                        if (this.autoRound == "On") {
                             this.blinkLights();
                         }
                     }
                 }
-                else{
-                    this.gameover=true;
+                else {
+                    this.gameover = true;
                     this.lights[index].color = 'red';
                     this.GameOver();
                 }
             }
         },
-        GameOver(){
+        GameOver() {
             if (this.user) {
                 const uid = this.user.uid;
                 const date = new Date();
-                const datepath = date.getDate()+'q'+date.getMonth()+'q'+date.getFullYear()+'e'+date.getHours()+'w'+date.getMinutes()+'w'+date.getSeconds(); 
+                var datepath = "";
+                if (date.getDate() < 10) {
+                    datepath += "0" + date.getDate();
+                }
+                else {
+                    datepath += date.getDate();
+                }
+                datepath += 'q';
+                if (date.getMonth() + 1 < 10) {
+                    datepath += "0" + (date.getMonth() + 1);
+                }
+                else {
+                    datepath += (date.getMonth() + 1);
+                }
+                datepath += 'q';
+                datepath += date.getFullYear();
+                datepath += 'e';
+
+                if (date.getHours() < 10) {
+                    datepath += "0" + date.getHours();
+                }
+                else {
+                    datepath += date.getHours();
+                }
+                datepath += "w";
+                if (date.getMinutes() < 10) {
+                    datepath += "0" + date.getMinutes();
+                }
+                else {
+                    datepath += date.getMinutes();
+                }
+                datepath += "w";
+                if (date.getSeconds() < 10) {
+                    datepath += "0" + date.getSeconds();
+                }
+                else {
+                    datepath += date.getSeconds();
+                }
                 // q = '-'
                 // e = ' '
                 // w = ':'
 
-                const score = this.sequenceLength-1
+                const score = this.sequenceLength - 1
                 const path = `/users/${uid}/simonsaysScores/${datepath}`;
 
                 // Add the timer value under the "simonsays" subfolder
                 set(ref(db, path), score).then(() => {
-                // Data added successfully
+                    // Data added successfully
                 }).catch((error) => {
-                console.error('Error adding data:', error);
+                    console.error('Error adding data:', error);
                 });
 
 
-                if(this.user.emailVerified){
+                if (this.user.emailVerified) {
                     const displayPath = `${this.user.uid}`;
-                    const latestPath = `/games/simonsays/latestScores/`+displayPath;
+                    const latestPath = `/games/simonsays/latestScores/` + displayPath;
 
                     // Add the timer value under the "latestScores" subfolder
                     set(ref(db, latestPath), score).then(() => {
-                    // Data added successfully
+                        // Data added successfully
                     }).catch((error) => {
-                    console.error('Error adding data:', error);
+                        console.error('Error adding data:', error);
                     });
 
 
@@ -203,9 +221,9 @@ export default {
                         if (currentScore - score < 0 || currentScore == null) {
                             // Update the value under the "highscores" subfolder
                             set(currentScoreRef, score).then(() => {
-                            // Data added successfully
+                                // Data added successfully
                             }).catch((error) => {
-                            console.error('Error updating data:', error);
+                                console.error('Error updating data:', error);
                             });
                         }
                     }).catch((error) => {
@@ -213,47 +231,47 @@ export default {
                     });
                 }
             }
-            else{
+            else {
                 console.log("No user logged in");
             }
         },
-        updateTopScores (){
+        updateTopScores() {
             const highScoresRef = ref(db, '/games/simonsays/highscores');
             const maxCount = 50;
 
             onValue(highScoresRef, (snapshot) => {
-            const data = snapshot.val();
+                const data = snapshot.val();
 
-            if (data) {
-                const dataArray = Object.entries(data).map(([key, value]) => ({ key, value }));
+                if (data) {
+                    const dataArray = Object.entries(data).map(([key, value]) => ({ key, value }));
 
-                // Sort the array in ascending order (lowest to highest)
-                dataArray.sort((a, b) => a.value.score - b.value.score);
+                    // Sort the array in ascending order (lowest to highest)
+                    dataArray.sort((a, b) => a.value.score - b.value.score);
 
-                if (dataArray.length > maxCount) {
-                const itemsToRemove = dataArray.slice(maxCount);
-                
-                itemsToRemove.forEach((item) => {
-                    const key = item.key;
-                    remove(ref(db, `/games/simonsays/highscores/${key}`));
-                });
+                    if (dataArray.length > maxCount) {
+                        const itemsToRemove = dataArray.slice(maxCount);
+
+                        itemsToRemove.forEach((item) => {
+                            const key = item.key;
+                            remove(ref(db, `/games/simonsays/highscores/${key}`));
+                        });
+                    }
                 }
-            }
-            });  
+            });
         },
-        reset(){
-            this.lights=[
+        reset() {
+            this.lights = [
                 { color: 'rgb(189, 189, 218)' },
                 { color: 'rgb(189, 189, 218)' },
                 { color: 'rgb(189, 189, 218)' },
-                { color: 'rgb(189, 189, 218)'}
+                { color: 'rgb(189, 189, 218)' }
             ];
-            this.currentScore= 0;
-            this.sequenceLength=1,
-            this.currentlyBlinking = true;
-            this.currentlyPlaying= false;
-            this.gameover= false;
-            this.sequence=[];
+            this.currentScore = 0;
+            this.sequenceLength = 1,
+                this.currentlyBlinking = true;
+            this.currentlyPlaying = false;
+            this.gameover = false;
+            this.sequence = [];
         },
         getLightStyle(light, index) {
 
@@ -270,21 +288,22 @@ export default {
 </script>
 
 <style scoped>
-
 @media (min-width: 769px) {
-    #box{
+    #box {
         max-width: 1200px;
         width: 90%;
         border: solid;
         margin: auto;
-        background-color:bisque;
+        background-color: bisque;
         border-radius: 20px;
     }
-    #lightBox{
+
+    #lightBox {
         display: flex;
         justify-content: center;
     }
-    #switchBox{
+
+    #switchBox {
         margin: auto;
         width: auto;
         display: flex;
@@ -292,14 +311,16 @@ export default {
         justify-content: space-around;
         /* border: solid; */
     }
-    #switch{
+
+    #switch {
         width: auto;
         margin: auto;
         /* border: solid; */
         align-self: center;
     }
 
-    .light, .disabledLight {
+    .light,
+    .disabledLight {
         margin: 1vw;
         width: 12%;
         height: 50px !important;
@@ -308,22 +329,27 @@ export default {
         text-align: center;
         font-family: 'TitleFont';
     }
-    .light{
+
+    .light {
         background-color: #ffffff;
     }
 }
 
 @media (max-width: 768px) {
     #lightBox {
-        max-width: 1200px; /* Set the max-width for the 2x2 grid */
+        max-width: 1200px;
+        /* Set the max-width for the 2x2 grid */
         width: 90%;
         border: solid;
         margin: auto;
         padding: 10px;
-        display: flex; /* Use flex container */
-        flex-wrap: wrap; /* Allow buttons to wrap to the next row */
+        display: flex;
+        /* Use flex container */
+        flex-wrap: wrap;
+        /* Allow buttons to wrap to the next row */
         justify-content: center;
-        align-items: center; /* Center vertically */
+        align-items: center;
+        /* Center vertically */
         background-color: bisque;
         border-radius: 20px;
         overflow: auto;
@@ -331,18 +357,20 @@ export default {
 
     .light,
     .disabledLight {
-        margin: 10px; /* Set margin to create space between buttons */
+        margin: 10px;
+        /* Set margin to create space between buttons */
         width: 35vw !important;
         height: 35vw !important;
         font-size: x-large;
         text-align: center;
         font-family: 'TitleFont';
     }
-    .light{
+
+    .light {
         background-color: #ffffff;
     }
 
-    #switchBox{
+    #switchBox {
         margin: 2vw auto auto auto;
         width: 90%;
         display: flex;
@@ -354,7 +382,8 @@ export default {
         background-color: bisque;
         padding: 1vw 0;
     }
-    #switch{
+
+    #switch {
         width: auto;
         margin: auto;
     }
