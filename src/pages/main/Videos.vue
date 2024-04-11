266ea392b0e12cdfccc96244e276182c1b3c1b33
Kristian Goystdal <krisgoy100@gmail.com>
2024-04-12 00:24:02 +0200

    Refactor Videos.vue component to include a switch for World Map Videos and Video Gallery

--- src/pages/main/Videos.vue
+++ src/pages/main/Videos.vue
@@ -1,22 +1,52 @@
 <template>
-    <!-- <DevelopmentComp></DevelopmentComp> -->
-    <VideoGallery></VideoGallery>
+  <div>
+    <div v-if="model">
+      <VideoGallery></VideoGallery>
+    </div>
+    <div v-else>
+      <WorldMapVideos></WorldMapVideos>
+    </div>
+
+    <div class="switch-container">
+      <span>World Map Videos</span>
+      <v-switch v-model="model" hide-details inset></v-switch>
+      <span>Video Gallery</span>
+    </div>
+  </div>
 </template>
-    
+
 <script>
-    import DevelopmentComp from '../../components/main/DevelopmentComp.vue'
-    import VideoGallery from '../../components/videos/VideoGallery.vue'
+  import DevelopmentComp from '../../components/main/DevelopmentComp.vue'
+  import VideoGallery from '../../components/videos/VideoGallery.vue'
+  import WorldMapVideos from '../../components/videos/WorldMapVideos.vue'
 
-    export default{
-        name: "VideosPage", 
-        components: {
-            DevelopmentComp,
-            VideoGallery,
-        }
+  export default {
+    name: 'VideosPage',
+    components: {
+      DevelopmentComp,
+      VideoGallery,
+      WorldMapVideos
+    },
+    data() {
+      return {
+        model: false
+      }
     }
+  }
 </script>
-    
-<style scoped>
 
+<style scoped>
+  .switch-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+    width: fit-content;
+    margin: auto;
+    border: solid;
+  }
+
+  .switch-container > span {
+    padding: 0 20px; /* Adds spacing around the text */
+  }
 </style>
-    
